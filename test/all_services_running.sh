59b350028185dfb8026a602020fc7d83ef7bd3fa
Rune K. Svendsen <runesvend@gmail.com>
2021-01-15 21:35:26 +0100

    ready to try out

--- test/all_services_running.sh
+++ test/all_services_running.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
-RUNNING_SERVICES=$(docker-compose ps | grep Up | awk '{print $1}')
-ALL_SERVICES="liquidity-database liquidity-service-create liquidity-service-process liquidity-web-api orderbook-service test-service"
+# RUNNING_SERVICES=$(docker-compose ps | grep Up | awk '{print $1}')
+RUNNING_SERVICES=$(cat test-output.txt)
+ALL_SERVICES="database liquidity-service-create liquidity-service-process liquidity-web-api orderbook-service test-service"
 
 # Iterate the string variable using for loop
 for service in $ALL_SERVICES; do
-    IS_RUNNING=$(echo $RUNNING_SERVICES | grep "$service")
-    [ -z "$IS_RUNNING" ] && echo "Missing service: $service. Running services:" && echo "$RUNNING_SERVICES" && exit 1
+    IS_RUNNING=$(echo "$RUNNING_SERVICES" | grep "$service")
+    if [ -z "$IS_RUNNING" ]; then
+        echo "Missing service: $service. Running services:"
+        echo "$RUNNING_SERVICES"
+        exit 1
+    else
+        echo "Service is running: \"$service\" (container: $IS_RUNNING)"
+    fi
 done
+
+exit 0
